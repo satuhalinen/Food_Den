388392993b5b6b881c7c468f00c31b5187c361b4
[satuhalinen] <satuh9@gmail.com>
2023-11-09 16:03:37 +0200

    add foodMenu

---
+++ backend/foodMenu.js
@@ -0,0 +1,73 @@
+const foodMenu = [
+  {
+    name: "Cake",
+    image:
+      "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=anna-tukhfatullina-food-photographer-stylist-Mzy-OjtCI70-unsplash.jpg",
+    description: "Rasberry cake with fresh rasperries",
+    price: 6,
+  },
+  {
+    name: "Fish",
+    image:
+      "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=ca-creative-bpPTlXWTOvg-unsplash.jpg",
+    description: "Pan-fried salmon with seasonal vegetables",
+    price: 25,
+  },
+  {
+    name: "Hamburger",
+    image:
+      "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=food-photographer-E94j3rMcxlw-unsplash.jpg",
+    description: "Beef burger with bacon",
+    price: 18,
+  },
+  {
+    name: "Pancakes",
+    image:
+      "https://images.unsplash.com/photo-1600326145552-327f74b9c189?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=joshua-ryder-y4K7Ydp2z3Y-unsplash.jpg",
+    description:
+      "Fluffy pancakes with strawberries, bilberries and maple syrup",
+    price: 10,
+  },
+  {
+    name: "Pasta",
+    image:
+      "https://images.unsplash.com/photo-1567608285969-48e4bbe0d399?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=victoria-aleksandrova-n56ZKeW9bVM-unsplash.jpg",
+    description: "Avocado pasta",
+    price: 16,
+  },
+  {
+    name: "Pizza",
+    image:
+      "https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=vita-marija-murenaite-eSeo6IzOV00-unsplash.jpg",
+    description: "Mozzarella pizza with rucola",
+    price: 16,
+  },
+  {
+    name: "Sallad",
+    image:
+      "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=eiliv-aceron-ZuIDLSz3XLg-unsplash.jpg",
+    description: "Feta sallad with walnuts and olives",
+    price: 15,
+  },
+  {
+    name: "Soup",
+    image:
+      "https://images.unsplash.com/photo-1605909388460-74ec8b204127?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=jezebel-rose-KGw62KtHzxA-unsplash.jpg",
+    description: "Creamy vegetable soup",
+    price: 15,
+  },
+  {
+    name: "Steak",
+    image:
+      "https://images.unsplash.com/photo-1546833160-1ae25ca1a6d5?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=krystel-heddy-GUAxf0tMcKU-unsplash.jpg",
+    description: "Grilled steak with mushrooms",
+    price: 24,
+  },
+  {
+    name: "Sushi",
+    image:
+      "https://images.unsplash.com/photo-1676037150304-e4c4a1d585f7?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=orkun-orcan-38siyimVVUA-unsplash.jpg",
+    description: "Roe sushi with soy sauce",
+    price: 20,
+  },
+];
