bb2de6f28eff40a865979ad7a1a9acd13828bfcc
George Uzoh <ugochukwuuzoh@gmail.com>
2023-12-14 14:48:26 +0200

    add cart sample

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Card from "./components/Card";
 import "./App.css";
 import Carousel from "react-multi-carousel";
 import { foodMenu, responsive } from "./data";
+import Swal from 'sweetalert2'
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -13,6 +14,28 @@ function App() {
   const handleSearch = (e) => {
     const searchString = e.target.value.toLowerCase();
     setSearchQuery(searchString);
+  }
+
+  let cartItems = [];
+
+  const addToCart = (name, price) => {
+    cartItems.push({name, price});
+    
+    let timerInterval;
+    Swal.fire({
+      icon: 'success',
+      title: `${name} added to cart`,
+      timer: 2000,
+      didOpen: () => timerInterval = setInterval(() => {}, 1000),
+      willClose: () => clearInterval(timerInterval),
+    });
+  }
+
+  const showCart = () => {
+    const cart = cartItems.length > 0
+      ? cartItems
+      : 'Cart is empty';
+    console.log(cart);
   }
 
   const details = foodMenu
@@ -23,12 +46,13 @@ function App() {
         url={item.image}
         price={item.price}
         description={item.description}
+        addToCart={() => addToCart(item.name, item.price)}
       />
     ));
 
   return (
     <>
-      <Header handleSearch={handleSearch} />
+      <Header handleSearch={handleSearch} showCart={showCart} />
       <div className="heroImage">
         <h1>Delicious food is just a click away</h1>
         <p className="onPictureText">
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -25,7 +25,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=anna-tukhfatullina-food-photographer-stylist-Mzy-OjtCI70-unsplash.jpg",
       description: "Rasberry cake with fresh rasperries",
-      price: "€6",
+      price: 6,
       id: 1,
     },
     {
@@ -33,7 +33,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=ca-creative-bpPTlXWTOvg-unsplash.jpg",
       description: "Pan-fried salmon with seasonal vegetables",
-      price: "€25",
+      price: 25,
       id: 2,
     },
     {
@@ -41,7 +41,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=food-photographer-E94j3rMcxlw-unsplash.jpg",
       description: "Beef burger with bacon",
-      price: '€18',
+      price: 18,
       id: 3,
     },
     {
@@ -50,7 +50,7 @@ export const foodMenu = [
         "https://images.unsplash.com/photo-1600326145552-327f74b9c189?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=joshua-ryder-y4K7Ydp2z3Y-unsplash.jpg",
       description:
         "Fluffy pancakes with strawberries, bilberries and maple syrup",
-      price: '€10',
+      price: 10,
       id: 4,
     },
     {
@@ -58,7 +58,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1567608285969-48e4bbe0d399?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=victoria-aleksandrova-n56ZKeW9bVM-unsplash.jpg",
       description: "Avocado pasta",
-      price: '€16',
+      price: 16,
       id: 5,
     },
     {
@@ -66,7 +66,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=vita-marija-murenaite-eSeo6IzOV00-unsplash.jpg",
       description: "Mozzarella pizza with rucola",
-      price: '€16',
+      price: 16,
       id: 6,
     },
     {
@@ -74,7 +74,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=eiliv-aceron-ZuIDLSz3XLg-unsplash.jpg",
       description: "Feta sallad with walnuts and olives",
-      price: '€15',
+      price: 15,
       id: 7,
     },
     {
@@ -82,7 +82,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1605909388460-74ec8b204127?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=jezebel-rose-KGw62KtHzxA-unsplash.jpg",
       description: "Creamy vegetable soup",
-      price: '€15',
+      price: 15,
       id: 8,
     },
     {
@@ -90,7 +90,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1546833160-1ae25ca1a6d5?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=krystel-heddy-GUAxf0tMcKU-unsplash.jpg",
       description: "Grilled steak with mushrooms",
-      price: '€24',
+      price: 24,
       id: 9,
     },
     {
@@ -98,7 +98,7 @@ export const foodMenu = [
       image:
         "https://images.unsplash.com/photo-1676037150304-e4c4a1d585f7?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=orkun-orcan-38siyimVVUA-unsplash.jpg",
       description: "Roe sushi with soy sauce",
-      price: '€20',
+      price: 20,
       id: 10,
     },
   ];
