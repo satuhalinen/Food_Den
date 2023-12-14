b00c1d176a06a115eb8608dd0a52dc8870285a27
[satuhalinen] <satuh9@gmail.com>
2023-12-14 17:15:04 +0200

    make footer and main UI better

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,12 @@ import { foodMenu, responsive } from "./data";
 import Swal from 'sweetalert2'
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = (e) => {
     const searchString = e.target.value.toLowerCase();
     setSearchQuery(searchString);
-  }
+  };
 
   let cartItems = [];
 
@@ -39,9 +39,10 @@ function App() {
   }
 
   const details = foodMenu
-    ?.filter(item => item.name.toLowerCase().includes(searchQuery))
-    ?.map((item) => (
+    ?.filter((item) => item.name.toLowerCase().includes(searchQuery))
+    ?.map((item, i) => (
       <Card
+        key={i}
         name={item.name}
         url={item.image}
         price={item.price}
@@ -52,43 +53,42 @@ function App() {
 
   return (
     <>
-      <Header handleSearch={handleSearch} showCart={showCart} />
-      <div className="heroImage">
-        <h1>Delicious food is just a click away</h1>
-        <p className="onPictureText">
-          Whether you're craving the comfort of home-cooked meals, exploring
-          exotic cuisines, or seeking a quick bite on a busy day, we've curated
-          a menu that caters to every palate.
-        </p>
-      </div>
 
-      <div className="aboutUs">
-        <h2>About us</h2>
-        <p className="story">
-          Welcome to Food Den! your passport to flavor paradise! We're on a
-          mission to bring exceptional food straight to your doorstep,
-          hassle-free. Dive into our curated menu featuring top-notch dishes
-          from local gems to international delights. Ordering is a breeze, and
-          our speedy delivery ensures your meal is hot and fresh. Join us in
-          celebrating good food, good times, and the ease of dining with Food
-          Den. Cheers to savoring the moment!
-        </p>
-      </div>
+      <div className="firstViewPort">
+        <Header handleSearch={handleSearch} showCart={showCart} />
+        <div className="heroImage">
+          <h1>Delicious food is just a click away</h1>
+          <p className="onPictureText">
+            Whether you're craving the comfort of home-cooked meals, exploring
+            exotic cuisines, or seeking a quick bite on a busy day, we've
+            curated a menu that caters to every palate.
+          </p>
+        </div>
 
-      <div className="foodCard">
-      <h2 className="menu">Menu</h2>
-      <Carousel responsive={responsive}>{details}</Carousel>
       </div>
-     
-      <Footer />
+      <div className="secondViewPort">
+        <div className="aboutUs">
+          <h2>About us</h2>
+          <p className="story">
+            Welcome to Food Den! your passport to flavor paradise! We're on a
+            mission to bring exceptional food straight to your doorstep,
+            hassle-free. Dive into our curated menu featuring top-notch dishes
+            from local gems to international delights. Ordering is a breeze, and
+            our speedy delivery ensures your meal is hot and fresh. Join us in
+            celebrating good food, good times, and the ease of dining with Food
+            Den. Cheers to savoring the moment!
+          </p>
+        </div>
+
+        <div className="foodCard">
+          <h2 className="menu">Menu</h2>
+          <Carousel responsive={responsive}>{details}</Carousel>
+        </div>
+
+        <Footer />
+      </div>
     </>
   );
 }
 
 export default App;
-
-
-
-
-
-
