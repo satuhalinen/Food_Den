a2d86afe9bf7f5d00a8b173acae368115c00c3c0
George Uzoh <ugochukwuuzoh@gmail.com>
2023-12-16 16:39:59 +0200

    add appreciation text

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,28 +2,31 @@ const Cart = ({ cartItems }) => {
   return (
     <div>
         {cartItems.length > 0
-            ? <table>
-                <thead>
-                    <tr>
-                        <th className="heading">Item</th>
-                        <th className="heading">Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cartItems.map((item, i) => (
-                        <tr key={i}>
-                            <td>{item.name}</td>
-                            <td>{item.price} €</td>
+            ? <div>
+                <p>Thanks for your patronage. Here&apos;s a summary of your order</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="heading">Item</th>
+                            <th className="heading">Price</th>
                         </tr>
-                    ))}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td className="total">Total</td>
-                        <td className="total">{cartItems.reduce((prev, curr) => prev + curr.price, 0)} €</td>
-                    </tr>
-                </tfoot>
-            </table> 
+                    </thead>
+                    <tbody>
+                        {cartItems.map((item, i) => (
+                            <tr key={i}>
+                                <td>{item.name}</td>
+                                <td>{item.price} €</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td className="total">Total</td>
+                            <td className="total">{cartItems.reduce((prev, curr) => prev + curr.price, 0)} €</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div> 
             : <p>Cart is empty</p>
         }
     </div>
