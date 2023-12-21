3d66bf62bcfe86889dea54ae80e92d3677ae70ca
[satuhalinen] <satuh9@gmail.com>
2023-12-21 16:55:30 +0200

    modify footer and header to deploy in Netlify

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <footer>
         <div>
-          <img src="./src/assets/logo.png" alt="logo" className="footerLogo" />
+          <img src="/src/assets/logo.png" alt="logo" className="footerLogo" />
         </div>
         <small>Copyright © 2023 GAS Coders</small>
       </footer>
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -4,19 +4,15 @@ const Header = () => {
   return (
     <header>
       <Link to="/">
-        <img src="./src/assets/logo.png" alt="logo" className="logo" />
+        <img src="/src/assets/logo.png" alt="logo" className="logo" />
       </Link>
 
       <div>
         <Link to="/menu">
-          <button className="btn">
-            Menu
-          </button>
+          <button className="btn">Menu</button>
         </Link>
         <Link to="/about">
-          <button className="btn">
-            About us
-          </button>
+          <button className="btn">About us</button>
         </Link>
       </div>
       <Link to="/cart">
